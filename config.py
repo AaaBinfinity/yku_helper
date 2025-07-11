027ef5a1510dd995def006d6f3b61ee86656c687
Binfinity <binfinity@qq.com>
2025-07-11 17:12:00 +0800

    配置

--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 from dotenv import load_dotenv
 import os
 
-# ✅ 加载 .env 文件（必须调用）
+# 加载 .env 文件
 load_dotenv()
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 # AI 配置
 API_KEY = os.getenv("API_KEY")
 AI_MODEL_NAME = os.getenv("AI_MODEL_NAME")
-HOST = "ark.cn-beijing.volces.com"
-PATH = "/api/v3/chat/completions"
+HOST = os.getenv("HOST")
+PATH = os.getenv("PATH")
 
-
-
+# 数据库配置
 DB_CONFIG = {
-    "host": "1.92.109.205",
-    "user": "binfinity",
-    "password": "Cb050328_password",
-    "database": "ykuhelp",
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
     "charset": "utf8mb4"
 }
